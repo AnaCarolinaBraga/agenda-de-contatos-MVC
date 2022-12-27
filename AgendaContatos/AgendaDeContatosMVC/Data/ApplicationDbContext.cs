3fa6c7afc2a1152c3e16c1687267581d40ade022
Ana Carolina Braga <anabraga95@gmail.com>
2022-12-27 18:15:56 -0300

    Add Entity Framework & SQL Server Connection
    add-migration
    update-database

---
+++ AgendaContatos/AgendaDeContatosMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AgendaDeContatosMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaDeContatosMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Contact> AgContacts { get; set; }
+    }
+}
--- AgendaContatos/AgendaDeContatosMVC/Models/Contact.cs
+++ AgendaContatos/AgendaDeContatosMVC/Models/Contact.cs
@@ -1,6 +1,21 @@
-﻿namespace AgendaDeContatosMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaDeContatosMVC.Models
 {
     public class Contact
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string EMail { get; set; }
+
+        public int Phone { get; set; }
+
+        public string Adress { get; set; }
+
+        public DateTime Birthday { get; set; }
     }
 }
